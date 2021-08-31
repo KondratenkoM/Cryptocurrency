f1fe56ff71fd8292ba9195e5102d0c37ac362e92
Maxim <m0536k@gmail.com>
2021-08-31 12:33:38 +0300

    Initial Commit

---
+++ cryptocurrency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cryptocurrency
+//
+//  Created by apple on 31.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
